e3e4cc0efa8a713cda83762af97eba6c72bbb268
Gg123 <li.c@mail.ru>
2021-03-13 16:30:44 +0300

    3.2. Фиксация изменений на github [#470227]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Galina Ponomareva");
+        System.out.println("11.03.2021");
+    }
+}
