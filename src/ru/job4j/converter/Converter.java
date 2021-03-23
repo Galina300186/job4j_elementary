b0abe96a32baab3cb2d3a131445764d3f9471917
Gg123 <li.c@mail.ru>
2021-03-24 02:14:25 +0300

    3.2. Фиксация изменений на github [#470227]  Аргументы [#470211]  Конвертер валюты [#470199]  Идеальный вес [#470200]  Расстояние между точками в системе координат [#470214]  Стороны прямоугольника. [#470203]  Тестирование

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,10 +13,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        int dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int in1 = 180;
 
+        int expected = 2;
+        int expected1 = 3;
+        int out = Converter.rubleToEuro(in);
+        int out1 = Converter.rubleToDollar(in1);
+        boolean passed = expected == out;
+        boolean passed1 = expected1 == out1;
+
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("180 rubles are 3. Test result : " + passed1);
     }
 }
