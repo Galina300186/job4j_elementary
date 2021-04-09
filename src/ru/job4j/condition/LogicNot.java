afc032c702524589ca3f7f16564e255f18645fbf
Gg123 <li.c@mail.ru>
2021-04-09 18:15:16 +0300

    3.2. Фиксация изменений на github [#470227]  Аргументы [#470211]  Конвертер валюты [#470199]  Идеальный вес [#470200]  Расстояние между точками в системе координат [#470214]  Стороны прямоугольника. [#470203]  Тестирование  Cinema  Club  Max  LogicNot

---
+++ src/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return !isEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || !isPositive(num);
+    }
+
+    public static void main(String[] args) {
+        boolean result1=LogicNot.isEven(-8);
+        boolean result2=LogicNot.isPositive(40);
+        boolean result3=LogicNot.notEven(-90);
+        boolean result4=LogicNot.notPositive(6);
+        boolean result5=LogicNot.notEvenAndPositive(77);
+        boolean result6=LogicNot.evenOrNotPositive(32);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+    }
+}
+
