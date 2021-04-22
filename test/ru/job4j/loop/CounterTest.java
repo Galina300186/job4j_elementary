ed0665f63a48d09dbb58016e79e7672638248ca0
Gg123 <li.c@mail.ru>
2021-04-23 00:26:52 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        Assert.assertEquals(expected, result);
+    }
+}
