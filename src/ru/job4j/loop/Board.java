bae3d97ea32fb57711c325503c59afe33aee5549
Gg123 <li.c@mail.ru>
2021-04-23 17:18:45 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -2,14 +2,14 @@ package ru.job4j.loop;
 
 public class Board {
     public static void paint(int width, int height) {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+        for (int row = 0; row < width; row++) {
+            for (int cell = 0; cell < height; cell++) {
                 /* условие проверки, что писать пробел или X */
                 /* Выше в задании мы определили закономерность, когда нужно проста,влять X */
-                if (x % 2 == 0) {
+                if ((row + cell) % 2 == 0) {
                     System.out.print("X");
                 } else {
-                    System.out.print("Y");
+                    System.out.print("");
                 }
             }
             /* добавляем перевод на новую строку. */
