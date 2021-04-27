a1972acde6e28dc44fcec12f7bf148e5b13f12d6
Gg123 <li.c@mail.ru>
2021-04-27 15:41:39 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- test/ru/job4j/loop/Mortgage.java
+++ test/ru/job4j/loop/Mortgage.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double k = percent / 100;
-        int d = amount;
         while (amount >= 0) {
             year +=  1;
-            amount *= k;
-            amount += d;
-            amount -= salary;
+            amount +=  (amount * percent / 100 - salary);
         }
         return year;
     }
 }
+
