f694386131b70c9f88022dfa172d19145dee3f94
Gg123 <li.c@mail.ru>
2021-04-30 17:06:33 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,23 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        float[] prices = new float[40];
         short[] ages = new short[10];
         String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan Petrov";
+        names[1] = "james Bond";
+        names[2] = "Sergei Ponomarev";
+        names[3] = "Ostap Bender";
+        String people = names[0];
+        System.out.println(people);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         System.out.println("Размер массива равен " + ages.length);
         System.out.println("Размер массива равен " + surnames.length);
         System.out.println("Размер массива равен " + prices.length);
+
 
     }
 }
