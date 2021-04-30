56e88bfdfd4632e5f738db76a24ce781cd1f0084
Gg123 <li.c@mail.ru>
2021-04-30 16:24:32 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,7 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
-        float [] prices = new float[40];
+        float[] prices = new float[40];
 
     }
 }
