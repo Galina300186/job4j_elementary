5fe68b125b46bab5a023b173f24fb38a40d64270
Gg123 <li.c@mail.ru>
2021-06-01 13:28:31 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -4,7 +4,8 @@ public class Matrix {
     public static int[][] multiple(int size) {
         int[][] numbers = new int[size][size];
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers[i].length; j++) {
+            for (int j = 0; j < numbers
+                    [i].length; j++) {
                 numbers[i][j] = (i + 1) * (j + 1);
             }
         }
