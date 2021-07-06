1e42baa049a2da0ada213b3e7410f07086b1980d
Gg123 <li.c@mail.ru>
2021-07-06 18:10:46 +0300

    Merge remote-tracking branch 'origin/master' Counter.java

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -7,9 +7,8 @@ public class Defragment {
             if (array[point] == null) {
                 for (point = index + 1; point < array.length; point++) {
                     if (array[point] != null) {
-                        String temp = array[index];
                         array[index] = array[point];
-                        array[point] = temp;
+                        array[point] = null;
                         break;
                     }
                 }
